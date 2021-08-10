6f7c54dfb5520bf154fed28a8f8ee60e56c3d38f
huan6766 <271492388@qq.com>
2021-08-10 20:49:09 +0800

    使用v-model

--- src/components/Money/Nodes.vue
+++ src/components/Money/Nodes.vue
@@ -2,16 +2,19 @@
   <div>
     <label class="notes">
       <span class="name">备注:</span>
-      <input type="text" placeholder="在这里输入备注">
+      <input type="text" v-model="value" placeholder="在这里输入备注">
     </label>
   </div>
 
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Nodes'
-};
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+@Component
+export default class Nodes extends Vue {
+  value = ''
+}
 </script>
 
 <style lang="scss" scoped>
