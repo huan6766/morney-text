9d65065226eb10a64cc1d49bcbec4de042dbc43b
huan6766 <271492388@qq.com>
2021-08-17 20:14:36 +0800

    Label.vue使用vuex,抽离TagHelper作为mixin

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button @click="create">新增标签</button>
+      <button @click="createTag">新增标签</button>
     </div>
     <ul class="current">
       <li v-for="tag in tagList" :key="tag.id"
@@ -16,19 +16,21 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+import {mixins} from 'vue-class-component';
+import TagHelper from '@/mixins/TagHelper';
 
 @Component({
   computed: {
     tagList() {
-      return this.$store.state.tagList
+      return this.$store.state.tagList;
     }
   }
 })
-export default class Tags extends Vue {
+export default class Tags extends mixins(TagHelper) {
   selectedTags: string[] = [];
 
   created() {
-    this.$store.commit('fetchTags')
+    this.$store.commit('fetchTags');
   }
 
   toggle(tag: string) {
@@ -41,11 +43,7 @@ export default class Tags extends Vue {
     this.$emit('update:value', this.selectedTags);
   }
 
-  create() {
-    const name = window.prompt('请输入标签名');
-    if (!name) {return window.alert('标签名不能为空');}
-    this.$store.commit('createTag', name)
-  }
+
 }
 </script>
 
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -2,7 +2,7 @@
   <Layout>
     <div class="tags">
       <router-link class="tag" v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${tag.id}`">
-        <span>{{tag.name}}</span>
+        <span>{{ tag.name }}</span>
         <Icon name="right"></Icon>
       </router-link>
     </div>
@@ -16,55 +16,65 @@
 import Vue from 'vue'
 import {Component} from 'vue-property-decorator'
 import Button from '@/components/Button'
+import {mixins} from 'vue-class-component'
+import TagHelper from '@/mixins/TagHelper'
+
+
 
 
 
 
 @Component({
-  components: {Button}
-})
-export default class Labels extends Vue{
-  tags = []
-
-  createTag() {
-    const name = window.prompt('请输入标签名')
-    if(name) {
-      // store.createTag(name)
+  components: {Button},
+  computed: {
+    tags() {
+      return this.$store.state.tagList
     }
   }
+})
+export default class Labels extends mixins(TagHelper) {
+  beforeCreate() {
+    this.$store.commit('fetchTags')
+  }
+
+
 }
 </script>
 
 <style lang="scss" scoped>
-  .tags {
-    background: white;
-    font-size: 16px;
-    padding-left: 16px;
-    .tag {
-      min-height: 44px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      border-bottom: 1px solid #e6e6e6;
-      svg {
-        width: 18px;
-        height: 18px;
-        color: #666;
-        margin-right: 16px;
-      }
+.tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+
+  .tag {
+    min-height: 44px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #e6e6e6;
+
+    svg {
+      width: 18px;
+      height: 18px;
+      color: #666;
+      margin-right: 16px;
     }
   }
-  .createTag {
-    background: #767676;
-    color: white;
-    border-radius: 4px;
-    border: none;
-    height: 40px;
-    padding: 0 16px;
-    &-wrapper {
-      text-align: center;
-      padding: 16px;
-      margin-top: 44-16px;
-    }
+}
+
+.createTag {
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+
+  &-wrapper {
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
   }
+}
 </style>
