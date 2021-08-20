9745314ea4da24d4001542e8936cf89e077fdbf7
huan6766 <271492388@qq.com>
2021-08-20 15:11:13 +0800

    修复进页面就弹出4个提示的BUG

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -22,7 +22,8 @@ type TagListModel = {
 
 type RootState = {
   recordList: RecordItem[],
-  createRecordError: Error | numm
+  createRecordError: Error | null,
+  createTagError: Error | null,
   tagList: Tag[],
   currentTag?: Tag
 }
